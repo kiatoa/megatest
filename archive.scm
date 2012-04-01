28f853b5717cdae7d19e0224a7654ceff2001345
matt <matt>
2012-04-01 00:38:11 +0000

    Cleaned up ignores and added minimal example to docs

---
+++ archive.scm
@@ -0,0 +1,17 @@
+;; Copyright 2006-2011, Matthew Welland.
+;; 
+;;  This program is made available under the GNU GPL version 2.0 or
+;;  greater. See the accompanying file COPYING for details.
+;; 
+;;  This program is distributed WITHOUT ANY WARRANTY; without even the
+;;  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;;  PURPOSE.
+
+;;  strftime('%m/%d/%Y %H:%M:%S','now','localtime')
+
+(use sqlite3 srfi-1 posix regex regex-case srfi-69 dot-locking format)
+(import (prefix sqlite3 sqlite3:))
+
+(declare (unit archive))
+(declare (uses db))
+(declare (uses common))
