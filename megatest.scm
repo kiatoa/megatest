89c532506b0e72d777c3b26c56b2101d1de5c351
matt <matt>
2012-04-22 05:32:42 +0000

    Migrating from -test-paths to -test-path

--- megatest.scm
+++ megatest.scm
@@ -82,7 +82,7 @@ Test data capture
 Queries
   -list-runs patt         : list runs matching pattern \"patt\", % is the wildcard
   -showkeys               : show the keys used in this megatest setup
-  -test-paths targpatt    : get the most recent test path(s) matching targpatt e.g. %/%... 
+  -test-path targpatt    : get the most recent test path(s) matching targpatt e.g. %/%... 
                             returns list sorted by age ascending, see examples below
 
 Misc 
@@ -102,12 +102,10 @@ Spreadsheet generation
                             if it contains forward slashes the path will be converted
                             to windows style
 
-Helpers (these only apply in test run mode)
-
 Examples
 
-# Get test paths 
-megatest -test-paths -target ubuntu/n%/no% :runname w49% -testpatt test_mt%
+# Get test path, the '.' is required, could use '*' or a specific path/file
+megatest -test-path . -target ubuntu/n%/no% :runname w49% -testpatt test_mt%
 
 Called as " (string-intersperse (argv) " ")))
 
@@ -173,6 +171,7 @@ Called as " (string-intersperse (argv) " ")))
 			"-unlock"
 			;; queries
 			"-test-paths" ;; get path(s) to a test, ordered by youngest first
+			"-test-path"  ;; -test-paths is deprecated
 
 			"-runall"    ;; run all tests
 			"-remove-runs"
@@ -448,7 +447,7 @@ Called as " (string-intersperse (argv) " ")))
 ;; Get paths to tests
 ;;======================================================================
 ;; Get test paths matching target, runname, testpatt, and itempatt
-(if (args:get-arg "-test-paths")
+(if (or (args:get-arg "-test-path")(args:get-arg "-test-paths"))
     ;; if we are in a test use the MT_CMDINFO data
     (if (getenv "MT_CMDINFO")
 	(let* ((startingdir (current-directory))
@@ -470,7 +469,7 @@ Called as " (string-intersperse (argv) " ")))
 		(exit 1)))
 	  (if (not (setup-for-run))
 	      (begin
-		(debug:print 0 "Failed to setup, giving up on -test-paths, exiting")
+		(debug:print 0 "Failed to setup, giving up on -test-path, exiting")
 		(exit 1)))
 	  (set! db (open-db))    
 	  (if (not (args:get-arg "-server"))
@@ -485,8 +484,8 @@ Called as " (string-intersperse (argv) " ")))
 		      paths)))
 	;; else do a general-run-call
 	(general-run-call 
-	 "-test-paths"
-	 "Get paths to tests"
+	 "-test-path"
+	 "Get paths to test"
 	 (lambda (db target runname keys keynames keyvallst)
 	   (let* ((itempatt (args:get-arg "-itempatt"))
 		  (paths    (rdb:test-get-paths-matching db keynames target)))
