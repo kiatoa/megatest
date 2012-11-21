df5232daecef6d36a79f6bf2b9081683806847f9
matt <matt>
2012-11-21 03:25:44 +0000

    Added exception handling to deal with high cpu loads

--- common.scm
+++ common.scm
@@ -52,7 +52,7 @@
 (define *server-info*       #f)
 (define *time-to-exit*      #f)
 (define *received-response* #f)
-(define *default-numtries*  5)
+(define *default-numtries*  10)
 
 (define *target*            (make-hash-table)) ;; cache the target here; target is keyval1/keyval2/.../keyvalN
 (define *keys*              (make-hash-table)) ;; cache the keys here
