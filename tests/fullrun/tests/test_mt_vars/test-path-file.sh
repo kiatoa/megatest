1712b34cfc0aa72f4df4335205dd62aba8430ad5
mrwellan <mrwellan>
2012-12-13 04:25:16 +0000

    Fix for multiple return values from -test-paths

--- tests/fullrun/tests/test_mt_vars/test-path-file.sh
+++ tests/fullrun/tests/test_mt_vars/test-path-file.sh
@@ -4,20 +4,20 @@
 # get a previous test
 export EZFAILPATH=`$MT_MEGATEST -test-files envfile.txt -target $MT_TARGET :runname $MT_RUNNAME -testpatt runfirst/a%`
 
-echo Found $EZFAILPATH 
+echo "Found |$EZFAILPATH|"
 
-if [[ -e $EZFAILPATH/ ]];then
+if [ -e $EZFAILPATH ];then
   echo All good!
 else
   echo NOT good!
   exit 1
 fi
 
-export EZFAILPATH=`$MT_MEGATEST -test-paths -target $MT_TARGET :runname $MT_RUNNAME -testpatt runfirst/a%`
+export EZFAILPATH2=`$MT_MEGATEST -test-paths -target $MT_TARGET :runname $MT_RUNNAME -testpatt runfirst/a%`
 
-echo Found $EZFAILPATH
+echo "Found |$EZFAILPATH2|"
 
-if [[ -e $EZFAILPATH ]];then
+if [ -e $EZFAILPATH2 ];then
   echo All good!
 else
   echo NOT good!
