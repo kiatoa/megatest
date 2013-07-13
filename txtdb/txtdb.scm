cb264e26d7f71a55e8f84b3f1dd9005b64dc3479
matt <matt>
2013-07-13 06:01:00 +0000

    Some exporatory stuff for refdat editor

--- txtdb/txtdb.scm
+++ txtdb/txtdb.scm
@@ -9,6 +9,8 @@
 ;;  PURPOSE.
 
 (use ssax)
+(use sxml-serializer)
+(use sxml-modifications)
 
 ;; Read a non-compressed gnumeric file
 (define (txtdb:read-gnumeric-xml fname)
@@ -16,4 +18,34 @@
     (lambda ()
       (ssax:xml->sxml (current-input-port) '()))))
 
+ (define x (txtdb:read-gnumeric-xml "testdata-stripped.xml"))
+
+
+
+;; Write out sxml
+(with-output-to-file "testdata.sxml" (lambda()(pp x)))
+
+
 ;; (serialize-sxml a output: "new.xml")
+(with-output-to-file "testdata-stripped.xml" (lambda ()(print (sxml-serializer#serialize-sxml y))))
+
+;; Read in sxml file
+(with-input-from-file "testdata.sxml" (lambda ()(set! y (read))))
+
+(define (find-section dat section #!key (depth 0))
+  (let loop ((hed   (car dat))
+	     (tal   (cdr dat)))   
+    (if (list? hed)
+	(let ((res (find-section hed section depth: (+ depth 1))))
+	  (if res 
+	      res
+	      (if (null? tal)
+		  #f
+		  (loop (car tal)(cdr tal)))))
+	(if (eq? hed section)
+	    tal
+	    (if (null? tal)
+		#f
+		(loop (car tal)(cdr tal)))))))
+  
+(find-section x 'http://www.gnumeric.org/v10.dtd:Workbook)
