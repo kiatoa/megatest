50d6f95db8d8b4a28cf4dd74b1ab1b789a7b485f
matt <matt>
2013-10-13 23:59:05 +0000

    Part one of sqlite3 test done

--- dbwars/sqlite3-test.scm
+++ dbwars/sqlite3-test.scm
@@ -5,6 +5,7 @@
 (define db (open-database "test.db"))
 
 (execute db test-table-defn)
+(execute db syncsetup)
 
 (define (register-test db run-id testname host cpuload diskfree uname rundir shortdir item-path state status final-logf run-duration comment event-time)
   (execute db
@@ -12,3 +13,7 @@
 	   run-id
 	   testname host cpuload diskfree uname rundir shortdir item-path state status final-logf run-duration comment event-time))
 
+
+(create-tests db)
+
+(finalize! db)
