0a5cb7186a66ed25e8742944a3d8b9371a410c60
mrwellan <mrwellan>
2014-02-14 19:43:36 +0000

    More tweaks to the example

--- example/tests/diskspace/diskspace.sh
+++ example/tests/diskspace/diskspace.sh
@@ -1,16 +1,22 @@
 #!/bin/bash -e
 
-diskareas=`mount | egrep 'ext|mfs|nfs'|grep -v proc | awk '{print $3}'`
+filter=`refdb lookup $MT_RUN_AREA_HOME/$CFG_TYPE machines $TARGETHOST filter`
+
+echo "Using filter: $filter"
+
+diskareas=`mount | egrep 'ext|mfs|nfs'| egrep -v "$filter" | awk '{print $3}'`
 
 for dirname in $diskareas;do
 
+    echo "dirname: $dirname"
+
     # measure the free space
-    freespace=`df -k $dirname | grep $dirname | awk '{print $4}'`
+    freespace=`df -P -k $dirname | grep $dirname | awk '{print $4}'`
 
     # get the minfree allowed from the refdb
     minfree=`refdb lookup $MT_RUN_AREA_HOME/$CFG_TYPE machines $TARGETHOST minfree`
 
-    if [[ $freespace -lt $minfree ]];then
+    if [[ "$freespace" -lt "$minfree" ]];then
 	echo "ERROR: available space $freespace is less than minimum allowed of $minfree on $dirname"
     else
 	echo "INFO: space available of $freespace k on $dirname meets required minimum of $minfree."
