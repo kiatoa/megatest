eb5a80809bb4222a0ca4cc0af43d5779ea23572d
mrwellan <mrwellan>
2014-06-18 21:53:22 +0000

    Improvements to build for chicken/iup/sqlite3, see installck.sh

---
+++ utils/installck.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+myhome=$(dirname $0)
+
+if [[ $proxy == "" ]]; then 
+  echo 'Please set the environment variable "proxy" to host.com:port (e.g. foo.com:1234) to use a proxy'
+  echo PROX=""
+else
+  export http_proxy=http://$proxy
+  export PROX="-proxy $proxy"
+fi
+
+if [[ -z $PREFIX ]];then
+  echo "\$PREFIX variable is required"
+  exit
+fi
+
+logname=$(basename $PREFIX)
+
+script -c "make -f $myhome/Makefile_latest.installall all" $logname.log
