4560ea37a3e89074fea941e81dab64b4500fa72b
matt <matt>
2015-05-27 04:43:19 +0000

    Improved signal handling little more. Handle sigterm.

--- runs.scm
+++ runs.scm
@@ -224,23 +224,24 @@
 
     (if (tasks:need-server run-id)(tasks:start-and-wait-for-server tdbdat run-id 10))
 
-    (set-signal-handler! signal/int
-			 (lambda (signum)
-			   ;; (signal-mask! signum) ;; to mask or not? seems to cause issues in exiting
-			   (set! *time-to-exit* #t)
-			   (print "Received signal " signum ", cleaning up before exit. Please wait...")
-			   (let ((th1 (make-thread (lambda ()
-						     (let ((tdbdat (tasks:open-db)))
-						       (rmt:tasks-set-state-given-param-key task-key "killed"))
-						     (print "Killed by signal " signum ". Exiting")
-						     (exit))))
-				 (th2 (make-thread (lambda ()
-						     (thread-sleep! 3)
-						     (debug:print 0 "Done")
-						     (exit 4)))))
-			     (thread-start! th2)
-			     (thread-start! th1)
-			     (thread-join! th2))))
+    (let ((sighand (lambda (signum)
+		     ;; (signal-mask! signum) ;; to mask or not? seems to cause issues in exiting
+		     (set! *time-to-exit* #t)
+		     (print "Received signal " signum ", cleaning up before exit. Please wait...")
+		     (let ((th1 (make-thread (lambda ()
+					       (let ((tdbdat (tasks:open-db)))
+						 (rmt:tasks-set-state-given-param-key task-key "killed"))
+					       (print "Killed by signal " signum ". Exiting")
+					       (exit))))
+			   (th2 (make-thread (lambda ()
+					       (thread-sleep! 3)
+					       (debug:print 0 "Done")
+					       (exit 4)))))
+		       (thread-start! th2)
+		       (thread-start! th1)
+		       (thread-join! th2)))))
+      (set-signal-handler! signal/int sighand)
+      (set-signal-handler! signal/term sighand))
 
     ;; register this run in monitor.db
     (rmt:tasks-add "run-tests" user target runname test-patts task-key) ;; params)
