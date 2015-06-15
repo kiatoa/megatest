606440c832bfa9bf72b8343af0808ae4db52ed52
matt <matt>
2015-06-15 01:21:30 +0000

    Fixed couple issues with itemwait release test

--- tests/release/tests/itemwait/watchrun.sh
+++ tests/release/tests/itemwait/watchrun.sh
@@ -2,19 +2,25 @@
 
 runname=$1
 
-pass=1
-alldone=1
-while ! $alldone;do
+pass=no
+alldone=no
+while [[ $alldone == no ]];do
     sleep 5
-    bigrun_running=$(megatest -list-runs a|egrep 'bigrun\(.*RUNNING'|wc -l)
-    bigrun2_pass=$(megatest -list-runs a|egrep 'bigrun2.*COMPLETED.*PASS')
-    if [[ $bigrun_running -gt 0 && $bigrun2_pass -gt 0 ]];then
-	pass=0
-	alldone=0
+    $MTRUNNER $MTTESTDIR/fdktestqa/testqa $MTPATH megatest -list-runs $runname > list-runs.log
+    bigrun_running=$(cat list-runs.log | egrep 'bigrun\(.*RUNNING'|wc -l)
+    bigrun2_pass=$(cat list-runs.log   | egrep 'bigrun2.*COMPLETED.*PASS'|wc -l)
+    echo "bigrun_running=$bigrun_running, bigrun2_pass=$bigrun2_pass"
+    if [[ $bigrun_running -gt 0 ]] && [[ $bigrun2_pass -gt 0 ]];then
+	pass=yes
+	alldone=yes
+    fi
+    if [[ $bigrun_running -eq 0 ]];then
+	echo "bigrun all done and no bigrun2 found with PASS."
+	alldone=yes
     fi
 done
 
-if $pass;then
+if [[ $pass == yes ]];then
     echo PASS
     exit 0
 else
