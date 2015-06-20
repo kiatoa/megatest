be1876e3bc31c4560bd25d6ac4cf2df68d8ec50b
matt <matt>
2015-06-20 19:37:01 +0000

    Moved the sleep from server to client'

--- loadwatch/queuefeeder.scm
+++ loadwatch/queuefeeder.scm
@@ -18,7 +18,7 @@
 ;;   Connect to the server, the server delays the appropriate time (if 
 ;;   any) and then launch the task.
 ;;
-(use nanomsg posix regex)
+(use nanomsg posix regex message-digest md5)
 
 (define req   (nn-socket 'req))
 
@@ -42,6 +42,14 @@
   (nn-send soc msg)
   (nn-recv soc))
 
+;; Generate a unique signature for this client location
+;;
+(define (make-signature)
+  (message-digest-string (md5-primitive) 
+                         (with-output-to-string
+                           (lambda ()
+                             (write (current-directory))))))
+
 ;; (define ((talk-to-server soc))
 ;;   (let loop ((cnt 200000))
 ;;     (let ((name (list-ref '("Matt" "Tom" "Bob" "Jill" "James" "Jane")(random 6))))
@@ -54,14 +62,33 @@
 ;;   (exit))
 ;; 
 
-(thread-start! (lambda ()
-		 (thread-sleep! 60)
-		 (print "Give up on waiting for the server")
-		 (nn-close req)
-		 ;; (exit)
-		 ))
-(thread-join! (thread-start! (lambda ()
-			       (print (client-send-receive req (conc (current-user-name) "@" (get-host-name)))))))
+(define (get-delay signature)
+  (let* ((full-msg   (client-send-receive req (conc (current-user-name) "@" (get-host-name) ":" signature))))
+    (print "Got " full-msg)
+    (let* ((reply-msg  (string-match "^([\\d\\.]+)\\s+(.*)$" full-msg))
+	   (delay-time (if (> (length reply-msg) 2)
+			   (string->number (cadr reply-msg))
+			   1)) ;; fall back to one sec delay
+	   (msg        (if (> (length reply-msg) 2)
+			   (caddr reply-msg)
+			   full-msg)))
+      (values delay-time msg))))
+
+
+(let ((signature (make-signature)))
+  
+  (thread-start! (lambda ()
+		   (thread-sleep! 60)
+		   (print "Give up on waiting for the server")
+		   (nn-close req)
+		   ;; (exit)
+		   ))
+  (thread-join! (thread-start! (lambda ()
+				 (let-values 
+				  (((delay-time msg)(get-delay signature)))
+				  (print "INFO: sleeping " delay-time " seconds per request of queuefeeder server")
+				  (thread-sleep! delay-time)
+				  (print "INFO: done waiting, now executing requested task."))))))
 
 (process-execute (car cmd) (cdr cmd))
 
