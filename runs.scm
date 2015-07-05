63f50dd6402f7812e22659fc928c6a7fdbb9805d
matt <matt>
2015-07-05 22:58:25 +0000

    Fixed item based waiting.

--- runs.scm
+++ runs.scm
@@ -37,6 +37,8 @@
 
 ;; This is the *new* methodology. One record to inform them and in the chaos, organise them.
 ;;
+;; NOT YET UTILIZED
+;;
 (define (runs:create-run-record)
   (let* ((mconfig      (if *configdat*
 		           *configdat*
@@ -218,7 +220,7 @@
 	 (all-tests-registry #f)  ;; (tests:get-all)) ;; (tests:get-valid-tests (make-hash-table) test-search-path)) ;; all valid tests to check waiton names
 	 (all-test-names     #f)  ;; (hash-table-keys all-tests-registry))
 	 (test-names         #f)  ;; (tests:filter-test-names all-test-names test-patts))
-	 (required-tests     #f)  ;;(lset-intersection equal? (string-split test-patts ",") test-names))) ;; test-names)) ;; Added test-names as initial for required-tests but that failed to work
+	 (required-tests     #f)  ;; Put fully qualified test/testpath names in this list to be done
 	 (task-key           (conc (hash-table->alist flags) " " (get-host-name) " " (current-process-id)))
 	 (tdbdat             (tasks:open-db)))
 
@@ -261,6 +263,13 @@
     (set! test-names         (tests:filter-test-names all-test-names test-patts))
 
     ;; I think seeding required-tests with all test-names makes sense but lack analysis to back that up.
+
+    ;; NEW STRATEGY HERE:
+    ;; 1. fill required tests with test-patts
+    ;; 2. scan testconfigs and if waitons, itemwait, itempatt calc prior test test-patt
+    ;; 3. repeat until all deps propagated
+    
+    ;; any tests with direct mention in test-patts can be added to required
     ;;
     (set! required-tests     (lset-intersection equal? (string-split test-patts ",") all-test-names))
     ;; (set! required-tests     (lset-intersection equal? test-names all-test-names))
@@ -378,8 +387,22 @@
 	    (for-each 
 	     (lambda (waiton)
 	       (if (and waiton (not (member waiton test-names)))
-		   (begin
-		     (set! required-tests (cons waiton required-tests))
+		   (let ((new-test-patts (tests:extend-test-patts test-patts hed waiton #f)))
+		     ;; need to account for test-patt here, if I am test "a", selected with a test-patt of "hed/b%"
+		     ;; and we are waiting on "waiton" we need to add "waiton/,waiton/b%" to test-patt
+		     ;; is this satisfied by merely appending "/" to the waiton name added to the list?
+		     ;;
+		     ;; This approach causes all of the items in an upstream test to be run 
+
+		     (debug:print-info 0 "new-test-patts: " new-test-patts ", prev test-patts: " test-patts)
+		     (if (equal? new-test-patts test-patts)
+			 (set! required-tests (cons waiton required-tests)) ;; (cons (conc waiton "/") required-tests))
+			 (set! test-patts new-test-patts))
+		     
+		     ;; NOPE: didn't work. required needs to be plain test names. Try tacking on to test-patts
+		     ;;  - doesn't work
+		     ;; (set! test-patts (conc test-patts "," waiton "/"))
+		     
 		     (set! test-names (cons waiton test-names))))) ;; was an append, now a cons
 	     waitons)
 	    (let ((remtests (delete-duplicates (append waitons tal))))
@@ -387,7 +410,7 @@
 		  (loop (car remtests)(cdr remtests)))))))
 
     (if (not (null? required-tests))
-	(debug:print-info 1 "Adding " required-tests " to the run queue"))
+	(debug:print-info 1 "Adding \"" (string-intersperse required-tests " ") "\" to the run queue"))
     ;; NOTE: these are all parent tests, items are not expanded yet.
     (debug:print-info 4 "test-records=" (hash-table->alist test-records))
     (let ((reglen (configf:lookup *configdat* "setup" "runqueue")))
@@ -429,6 +452,7 @@
 		      (hash-table-set! flags "-preclean" #t))
 		  (if (not (hash-table-ref/default flags "-rerun" #f))
 		      (hash-table-set! flags "-rerun" "STUCK/DEAD,n/a,ZERO_ITEMS"))
+		  ;; recursive call to self
 		  (runs:run-tests target runname test-patts user flags run-count: (- run-count 1)))))
 	  (debug:print-info 0 "No tests to run")))
     (debug:print-info 4 "All done by here")
