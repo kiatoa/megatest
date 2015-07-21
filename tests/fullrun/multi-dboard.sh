2b56ea58dc3748dcb0546612c98fe3f9338031f9
matt <matt>
2015-07-21 03:35:09 +0000

    Improved debug in non-compiled work mode for multi-dboard

--- tests/fullrun/multi-dboard.sh
+++ tests/fullrun/multi-dboard.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-csi -I ../.. << EOF
-(require-library margs)
-(load "../../common.scm")
-(load "../../common_records.scm")
-(load "../../margs.scm")
-(load "../../megatest-version.scm")
-(load "../../portlogger.scm")
-(load "../../tasks.scm")
-(load "../../db.scm")
-(load "../../configf.scm")
-(load "../../keys.scm")
-(load "../../tree.scm")
-(load "../../multi-dboard.scm")
-EOF
+csi -I ../.. multi-dboard-load-all.scm
