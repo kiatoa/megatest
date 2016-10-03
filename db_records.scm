a6904707e90d3c8dea6e87111af1a86d0f534b84
srehman <srehman>
2016-10-03 21:40:22 +0000

    fixed db_records to account for modifications to test datatype update

--- db_records.scm
+++ db_records.scm
@@ -157,13 +157,13 @@
 ;; RADT => purpose of mintest??
 ;;
 (define (make-db:mintest)(make-vector 7))
-(define-inline (db:mintest-get-id           vec)    (vector-ref  vec 0))
-(define-inline (db:mintest-get-run_id       vec)    (vector-ref  vec 1))
-(define-inline (db:mintest-get-testname     vec)    (vector-ref  vec 2))
-(define-inline (db:mintest-get-state        vec)    (vector-ref  vec 3))
-(define-inline (db:mintest-get-status       vec)    (vector-ref  vec 4))
-(define-inline (db:mintest-get-event_time   vec)    (vector-ref  vec 5))
-(define-inline (db:mintest-get-item_path    vec)    (vector-ref  vec 6))
+(define-inline (db:mintest-get-id           vec)    (db:test-rec-id vec))
+(define-inline (db:mintest-get-run_id       vec)    (db:test-rec-run_id vec))
+(define-inline (db:mintest-get-testname     vec)    (db:test-rec-testname vec))
+(define-inline (db:mintest-get-state        vec)    (db:test-rec-state vec))
+(define-inline (db:mintest-get-status       vec)    (db:test-rec-status vec))
+(define-inline (db:mintest-get-event_time   vec)    (db:test-rec-event_time vec))
+(define-inline (db:mintest-get-item_path    vec)    (db:test-rec-item_path vec))
 
 ;; make-vector-record db testmeta id testname author owner description reviewed iterated avg_runtime avg_disk
 (define (make-db:testmeta)(make-vector 10 ""))
