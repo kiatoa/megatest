eb42e928804a38d046a715587fd781cd305bdce0
mrwellan <mrwellan>
2017-01-03 17:50:23 +0000

    Commented out a call to BB> that seemed to have trouble ...

--- megatest.scm
+++ megatest.scm
@@ -352,7 +352,7 @@ Version " megatest-version ", built from " megatest-fossil-hash ))
 (define *watchdog* (make-thread common:watchdog "Watchdog thread"))
 
 (thread-start! *watchdog*)
-(BB> "thread-start! watchdog")
+;; (BB> "thread-start! watchdog")
 (if (args:get-arg "-log")
     (let ((oup (open-output-file (args:get-arg "-log"))))
       (debug:print-info 0 *default-log-port* "Sending log output to " (args:get-arg "-log"))
