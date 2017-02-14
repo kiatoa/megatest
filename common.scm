30c965f41c7c4b4fdee2225e9ee1fbbbf2bdc47b
matt <matt>
2017-02-14 05:48:29 +0000

    Fixed few problems with actual launching of runs.

--- common.scm
+++ common.scm
@@ -853,15 +853,15 @@
   (or (args:get-arg "-status")(args:get-arg ":status")))
 
 (define (common:args-get-testpatt rconf)
-  (let* ((tagexpr (args:get-arg "-tagexpr"))
-         (tags-testpatt (if tagexpr (string-join (runs:get-tests-matching-tags tagexpr) ",") #f))
+  (let* (;; (tagexpr       (args:get-arg "-tagexpr"))
+         ;; (tags-testpatt (if tagexpr (string-join (runs:get-tests-matching-tags tagexpr) ",") #f))
          (testpatt-key  (if (args:get-arg "--modepatt") (args:get-arg "--modepatt") "TESTPATT"))
          (args-testpatt (or (args:get-arg "-testpatt") (args:get-arg "-runtests") "%"))
          (rtestpatt     (if rconf (runconfigs-get rconf testpatt-key) #f)))
     (cond
-     (tags-testpatt
-      (debug:print-info 0 *default-log-port* "-tagexpr "tagexpr" selects testpatt "tags-testpatt)
-      tags-testpatt)
+     ;; (tags-testpatt
+     ;;  (debug:print-info 0 *default-log-port* "-tagexpr "tagexpr" selects testpatt "tags-testpatt)
+     ;;  tags-testpatt)
      ((and (equal? args-testpatt "%") rtestpatt)
       (debug:print-info 0 *default-log-port* "testpatt defined in "testpatt-key" from runconfigs: " rtestpatt)
       rtestpatt)
--- runs.scm
+++ runs.scm
@@ -213,7 +213,8 @@
 	 (task-key           (conc (hash-table->alist flags) " " (get-host-name) " " (current-process-id)))
 	 (tdbdat             (tasks:open-db))
 	 (config-reruns      (let ((x (configf:lookup *configdat* "setup" "reruns")))
-			       (if x (string->number x) #f))))
+			       (if x (string->number x) #f)))
+	 (allowed-tests      #f))
 
     ;; per user request. If less than 100Meg space on dbdir partition, bail out with error
     ;; this will reduce issues in database corruption
@@ -255,6 +256,8 @@
 
     (if (not test-patts) ;; first time in - adjust testpatt
 	(set! test-patts (common:args-get-testpatt runconf)))
+    (if (args:get-arg "-tagexpr")
+	(set! allowed-tests (string-join (runs:get-tests-matching-tags (args:get-arg "-tagexpr")) ","))) ;; tests will be ANDed with this list
 
     ;; register this run in monitor.db
     (rmt:tasks-add "run-tests" user target runname test-patts task-key) ;; params)
@@ -263,7 +266,12 @@
     ;; Now generate all the tests lists
     (set! all-tests-registry (tests:get-all))   ;; hash of testname => path-to-test
     (set! all-test-names     (hash-table-keys all-tests-registry))
-    (set! test-names         (tests:filter-test-names all-test-names test-patts))
+    ;; filter first for allowed-tests (from -tagexpr) then for test-patts.
+    (set! test-names         (tests:filter-test-names
+			      (if allowed-tests
+				  (tests:filter-test-names all-test-names allowed-tests)
+				  all-test-names)
+			      test-patts))
 
     ;; I think seeding required-tests with all test-names makes sense but lack analysis to back that up.
 
