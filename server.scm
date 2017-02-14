a4ac06b6e18b62df6183be1b60d590a770401c91
mrwellan <mrwellan>
2017-02-14 23:44:14 +0000

    Added protection for call to get mod time

--- server.scm
+++ server.scm
@@ -185,7 +185,10 @@
 	      (let loop ((hed  (car server-logs))
 			 (tal  (cdr server-logs))
 			 (res '()))
-		(let* ((mod-time  (file-modification-time hed))
+		(let* ((mod-time  (handle-exceptions
+                                   exn
+                                   0
+                                   (file-modification-time hed))) ;; default to *very* old so log gets ignored if deleted
 		       (down-time (- (current-seconds) mod-time))
 		       (serv-dat  (if (or (< num-serv-logs 10)
 				  	  (< down-time day-seconds))
