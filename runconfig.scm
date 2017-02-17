0025275e5eee673e8cde48afff24506f78456b16
matt <matt>
2017-02-17 04:42:57 +0000

    Fixed wildcards in runconfigs, cleared some output debug noise.

--- runconfig.scm
+++ runconfig.scm
@@ -12,7 +12,7 @@
 
 (define (runconfig:read fname target environ-patt)
   (let ((ht (make-hash-table)))
-    (hash-table-set! ht target '())
+    (if target (hash-table-set! ht target '()))
     (read-config fname ht #t environ-patt: environ-patt sections: (if target (list "default" target) #f))))
 
 ;; NB// to process a runconfig ensure to use environ-patt with target!
