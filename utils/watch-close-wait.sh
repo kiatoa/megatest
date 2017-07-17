87340e0860bff89c1b814e328731aa72c2baa41a
mrwellan <mrwellan>
2017-07-17 16:42:02 +0000

    Fixed watch close wait script

--- utils/watch-close-wait.sh
+++ utils/watch-close-wait.sh
@@ -3,6 +3,6 @@ id=$(echo $psline|awk '{print $2}')
 echo "Watching process for command line: $psline"
 echo "  with PID=$id"
 while true;do
-  echo "$(lsof -n | grep CLOSE_WAIT | grep $id |grep CLOSE_WAIT | wc -l)"
+  echo "CLOSE_WAIT: $(lsof -n | grep CLOSE_WAIT | grep $id | wc -l) ALL OPEN: $(lsof -n |grep $id|wc -l) ALL CLOSE_WAIT: $(netstat -ap 2> /dev/null| grep -i close_wait| wc -l)"
   sleep 1
 done
