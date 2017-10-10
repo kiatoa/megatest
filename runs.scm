0cf4620d4b91a77a989f04f82439a10596d56b8f
mrwellan <mrwellan>
2017-10-10 00:06:47 +0000

    Lower priority on some debug messages

--- runs.scm
+++ runs.scm
@@ -674,7 +674,7 @@
 
 
          )
-    (debug:print-info 1 *default-log-port* "START OF INNER COND #2 "
+    (debug:print-info 4 *default-log-port* "START OF INNER COND #2 "
 		      "\n can-run-more:    " can-run-more
 		      "\n testname:        " hed
 		      "\n prereqs-not-met: " (runs:pretty-string prereqs-not-met)
@@ -1368,7 +1368,7 @@
 		  ;; (loop (car tal)(cdr tal) reg reruns))))
 
 	(runs:incremental-print-results run-id)
-	(debug:print 1 *default-log-port* "TOP OF LOOP => "
+	(debug:print 4 *default-log-port* "TOP OF LOOP => "
 		     "test-name: " test-name
 		     "\n  test-record  " test-record
 		     "\n  hed:         " hed
